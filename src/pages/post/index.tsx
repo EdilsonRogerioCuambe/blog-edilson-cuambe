3a2381514d050c7786888eb4120d53d17fbb6827
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-09-27 02:03:35 -0300

    update code

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -201,7 +201,7 @@ export function Post() {
               )
             },
           }}
-          rehypePlugins={[rehypeRaw as never]}
+          rehypePlugins={[rehypeRaw]}
         >
           {post.content}
         </ReactMarkdown>
